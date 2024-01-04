3e36c252827cb40e96f027fcb3ec48c1808c95aa
alMukaafih <almukaafih@gmail.com>
2024-01-04 12:28:18 +0100

    created icont

--- bin/icont.js
+++ bin/icont.js
@@ -4,10 +4,6 @@ const fs = require("fs");
 const os = require("os");
 const path = require("path");
 const AdmZip = require("adm-zip");
-const child_process = require("child_process");
-const { promisify } = require('util');
-const sleep = promisify(setTimeout);
-const { stylesGen } = require('../lib/libgen');
 
 // cleanup task
 process.on("exit", (code) => {
@@ -35,7 +31,6 @@ fs.symlinkSync("/data/data/com.termux/pj/vsacode", acode);
 // read extension/package.json file
 let _json = fs.readFileSync(path.join(tmpDir, "extension", "package.json"));
 let package_json = JSON.parse(_json);
-let version = package_json.version;
 let iconThemes = package_json.contributes.iconThemes;
 if (iconThemes == undefined)
     process.exit(1);
@@ -46,30 +41,5 @@ for (let iconTheme of iconThemes) {
     let _id = iconTheme.id;
     let _label = iconTheme.label;
     let _path = iconTheme.path;
-    let pwDir = path.join(tmpDir, "extension", _path);
-    let _json = fs.readFileSync(pwDir);
-    let icon_json = JSON.parse(_json);
-    let outDir = path.join(acode, "src");
-    fs.mkdirSync(outDir, { recursive: true });
-    
-    stylesGen(pwDir, outDir, icon_json);
-
-    let owd = process.cwd();
-    process.chdir(acode);
-    
-    // open plugin.json
-    console.log(`building: ${_label}`);
-    sleep(2000);
-    child_process.execSync(`nano ${path.join(acode, "plugin.json")}`, { stdio: 'inherit' });
-    child_process.execSync(`nano ${path.join(acode, "readme.md")}`, { stdio: 'inherit' });
-    child_process.execSync(`npm run build-release`, { stdio: 'inherit' });
-    child_process.execSync(`npm run clean`, { stdio: 'inherit' });
-    
-    
-    
-    let outZip = _id + "zip";
-    fs.copyFileSync(path.join(acode, "dist.zip"), path.join(owd, outZip));
-    console.log(`output: ${outZip}`);
-    x ++;
+    console.log(`${_label}: ${_id}`);
 }
-
